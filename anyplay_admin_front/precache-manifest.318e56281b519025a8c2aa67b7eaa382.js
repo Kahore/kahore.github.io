c646f8d5ed13c171722a73ffe5783094e5862bba
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2020-06-01 01:32:26 +0300

    new app

---
+++ anyplay_admin_front/precache-manifest.318e56281b519025a8c2aa67b7eaa382.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d7f9f2078bdfbe6927e65e19d5bbc3",
+    "url": "/anyplay_admin_front/index.html"
+  },
+  {
+    "revision": "04c0c0cdb3b3241a00e8",
+    "url": "/anyplay_admin_front/static/css/main.151ce7f5.chunk.css"
+  },
+  {
+    "revision": "8841d38855de1eaaae31",
+    "url": "/anyplay_admin_front/static/js/2.4a6d0b92.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/anyplay_admin_front/static/js/2.4a6d0b92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04c0c0cdb3b3241a00e8",
+    "url": "/anyplay_admin_front/static/js/main.c711d5b8.chunk.js"
+  },
+  {
+    "revision": "bb4e0cba9b0e511b72ea",
+    "url": "/anyplay_admin_front/static/js/runtime-main.689dc50c.js"
+  },
+  {
+    "revision": "0adef81b85e81c64bff20b4dd58b5f99",
+    "url": "/anyplay_admin_front/static/media/plus.0adef81b.svg"
+  }
+]);
